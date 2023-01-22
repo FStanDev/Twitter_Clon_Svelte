b68aba1b0e07c34e0f3898592d4b9db3cd081080
Stan <stan@Mac-mini-de-Adminsitrator.local>
2023-01-22 19:45:30 +0100

    Add Message Component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { loggedUser, pb } from '../lib/auth';
 	import { onMount } from 'svelte';
 	import type { ListOfMessages, Message } from '../models/messages';
+	import MessageComp from './MessageComp.svelte';
 
 	let resultList: ListOfMessages;
 	export let messages: Message[] = [];
@@ -24,7 +25,7 @@
 {#if $loggedUser}
 	<h1>Welcome to Twitter_Clon</h1>
 	{#each messages as message}
-		<div>{message.message}</div>
+		<MessageComp mensaje={message} />
 	{/each}
 {:else}
 	<!--<meta http-equiv='refresh' content='0; URL=/login'>-->
